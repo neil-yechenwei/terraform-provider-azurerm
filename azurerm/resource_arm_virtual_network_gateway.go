4e299fd0402787dbcbe943d303d3a572c8bdd154
neil-yechenwei <v-cheye@microsoft.com>
2019-09-18 17:07:02 +0800

    update

--- azurerm/resource_arm_virtual_network_gateway.go
+++ azurerm/resource_arm_virtual_network_gateway.go
@@ -82,7 +82,7 @@ func resourceArmVirtualNetworkGateway() *schema.Resource {
 				Type:             schema.TypeString,
 				Required:         true,
 				DiffSuppressFunc: suppress.CaseDifference,
-				// This validator checks for all possible values for the SKU regardless of the attributes vpn_type and
+				// This validastor checks for all possible values for the SKU regardless of the attributes vpn_type and
 				// type. For a validation which depends on the attributes vpn_type and type, refer to the special case
 				// validators validateArmVirtualNetworkGatewayPolicyBasedVpnSku, validateArmVirtualNetworkGatewayRouteBasedVpnSku
 				// and validateArmVirtualNetworkGatewayExpressRouteSku.
