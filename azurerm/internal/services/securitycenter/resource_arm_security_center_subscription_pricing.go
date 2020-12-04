d599743c4b2181d09151ae186c607d9fd010195d
neil-yechenwei <yechenwei2007@126.com>
2020-12-04 18:48:59 +0800

    update code

--- azurerm/internal/services/securitycenter/resource_arm_security_center_subscription_pricing.go
+++ azurerm/internal/services/securitycenter/resource_arm_security_center_subscription_pricing.go
@@ -48,8 +48,8 @@ func resourceArmSecurityCenterSubscriptionPricing() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 				ValidateFunc: validation.StringInSlice([]string{
-					string(security.Free),
-					string(security.Standard),
+					string(security.PricingTierFree),
+					string(security.PricingTierStandard),
 				}, false),
 			},
 			"resource_type": {
