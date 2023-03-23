206eae31d1ee1cd0bf43a5d3805b9e56fbf5542b
neil-yechenwei <yechenwei2007@hotmail.com>
2023-03-23 17:06:52 +0800

    New Resource: azurerm_postgresql_hyperscale_node_configuration

---
+++ internal/services/postgresqlhsc/postgresql_hyperscale_node_configuration_resource.go
@@ -0,0 +1,229 @@
+package postgresqlhsc
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
+	"github.com/hashicorp/terraform-provider-azurerm/utils"
+	"time"
+
+	"github.com/hashicorp/go-azure-helpers/lang/response"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/postgresqlhsc/2022-11-08/configurations"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+)
+
+type PostgreSQLHyperScaleNodeConfigurationModel struct {
+	Name      string `tfschema:"name"`
+	ClusterId string `tfschema:"cluster_id"`
+	Value     string `tfschema:"value"`
+}
+
+type PostgreSQLHyperScaleNodeConfigurationResource struct{}
+
+var _ sdk.ResourceWithUpdate = PostgreSQLHyperScaleNodeConfigurationResource{}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) ResourceType() string {
+	return "azurerm_postgresql_hyperscale_node_configuration"
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) ModelObject() interface{} {
+	return &PostgreSQLHyperScaleNodeConfigurationResource{}
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
+	return configurations.ValidateNodeConfigurationID
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) Arguments() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{
+		"name": {
+			Type:         pluginsdk.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: validation.StringIsNotEmpty,
+		},
+
+		"cluster_id": {
+			Type:         pluginsdk.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: configurations.ValidateServerGroupsv2ID,
+		},
+
+		"value": {
+			Type:         pluginsdk.TypeString,
+			Required:     true,
+			ValidateFunc: validation.StringIsNotEmpty,
+		},
+	}
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) Attributes() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{}
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) Create() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			var model PostgreSQLHyperScaleNodeConfigurationModel
+			if err := metadata.Decode(&model); err != nil {
+				return fmt.Errorf("decoding: %+v", err)
+			}
+
+			client := metadata.Client.PostgreSQLHSC.ConfigurationsClient
+			clusterId, err := configurations.ParseServerGroupsv2ID(model.ClusterId)
+			if err != nil {
+				return err
+			}
+
+			id := configurations.NewNodeConfigurationID(clusterId.SubscriptionId, clusterId.ResourceGroupName, clusterId.ServerGroupsv2Name, model.Name)
+
+			locks.ByName(id.ServerGroupsv2Name, "azurerm_postgresql_hyperscale_cluster")
+			defer locks.UnlockByName(id.ServerGroupsv2Name, "azurerm_postgresql_hyperscale_cluster")
+
+			parameters := &configurations.ServerConfiguration{
+				Properties: &configurations.ServerConfigurationProperties{
+					Value:  model.Value,
+					Source: utils.String("user-override"),
+				},
+			}
+
+			if err := client.UpdateOnNodeThenPoll(ctx, id, *parameters); err != nil {
+				return fmt.Errorf("updating %s: %+v", id, err)
+			}
+
+			// TODO: Add restart logic
+
+			metadata.SetID(id)
+			return nil
+		},
+	}
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) Update() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.PostgreSQLHSC.ConfigurationsClient
+
+			id, err := configurations.ParseNodeConfigurationID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			locks.ByName(id.ServerGroupsv2Name, "azurerm_postgresql_hyperscale_cluster")
+			defer locks.UnlockByName(id.ServerGroupsv2Name, "azurerm_postgresql_hyperscale_cluster")
+
+			var model PostgreSQLHyperScaleNodeConfigurationModel
+			if err := metadata.Decode(&model); err != nil {
+				return fmt.Errorf("decoding: %+v", err)
+			}
+
+			resp, err := client.GetNode(ctx, *id)
+			if err != nil {
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			parameters := resp.Model
+			if parameters == nil {
+				return fmt.Errorf("retrieving %s: properties was nil", id)
+			}
+
+			if metadata.ResourceData.HasChange("value") {
+				parameters.Properties.Value = model.Value
+			}
+
+			if err := client.UpdateOnNodeThenPoll(ctx, *id, *parameters); err != nil {
+				return fmt.Errorf("updating %s: %+v", *id, err)
+			}
+
+			// TODO: Add restart logic
+
+			return nil
+		},
+	}
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) Read() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 5 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.PostgreSQLHSC.ConfigurationsClient
+
+			id, err := configurations.ParseNodeConfigurationID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			resp, err := client.GetNode(ctx, *id)
+			if err != nil {
+				if response.WasNotFound(resp.HttpResponse) {
+					return metadata.MarkAsGone(id)
+				}
+
+				return fmt.Errorf("retrieving %s: %+v", *id, err)
+			}
+
+			model := resp.Model
+			if model == nil {
+				return fmt.Errorf("retrieving %s: model was nil", id)
+			}
+
+			state := PostgreSQLHyperScaleNodeConfigurationModel{
+				Name:      id.NodeConfigurationName,
+				ClusterId: configurations.NewServerGroupsv2ID(id.SubscriptionId, id.ResourceGroupName, id.ServerGroupsv2Name).ID(),
+			}
+
+			if props := model.Properties; props != nil {
+				state.Value = props.Value
+			}
+
+			return metadata.Encode(&state)
+		},
+	}
+}
+
+func (r PostgreSQLHyperScaleNodeConfigurationResource) Delete() sdk.ResourceFunc {
+	return sdk.ResourceFunc{
+		Timeout: 30 * time.Minute,
+		Func: func(ctx context.Context, metadata sdk.ResourceMetaData) error {
+			client := metadata.Client.PostgreSQLHSC.ConfigurationsClient
+
+			id, err := configurations.ParseNodeConfigurationID(metadata.ResourceData.Id())
+			if err != nil {
+				return err
+			}
+
+			locks.ByName(id.ServerGroupsv2Name, "azurerm_postgresql_hyperscale_cluster")
+			defer locks.UnlockByName(id.ServerGroupsv2Name, "azurerm_postgresql_hyperscale_cluster")
+
+			resp, err := client.GetNode(ctx, *id)
+			if err != nil {
+				return fmt.Errorf("retrieving %s: %+v", id, err)
+			}
+
+			defaultValue := ""
+			if resp.Model != nil && resp.Model.Properties != nil && resp.Model.Properties.DefaultValue != nil {
+				defaultValue = *resp.Model.Properties.DefaultValue
+			}
+
+			parameters := &configurations.ServerConfiguration{
+				Properties: &configurations.ServerConfigurationProperties{
+					Value:  defaultValue,
+					Source: utils.String("user-override"),
+				},
+			}
+
+			if err = client.UpdateOnNodeThenPoll(ctx, *id, *parameters); err != nil {
+				return fmt.Errorf("deleting %s: %+v", id, err)
+			}
+
+			// TODO: Add restart logic
+
+			return nil
+		},
+	}
+}
