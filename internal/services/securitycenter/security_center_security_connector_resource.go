b9d3d074f3bb65fb6c8a6d02b409afdd470b62ff
neil-yechenwei <yechenwei2007@hotmail.com>
2024-03-19 14:32:26 +0800

    update code

--- internal/services/securitycenter/security_center_security_connector_resource.go
+++ internal/services/securitycenter/security_center_security_connector_resource.go
@@ -19,12 +19,25 @@ import (
 )
 
 type SecurityCenterSecurityConnectorModel struct {
-	Name                string            `tfschema:"name"`
-	ResourceGroupName   string            `tfschema:"resource_group_name"`
-	Location            string            `tfschema:"location"`
-	EnvironmentName     string            `tfschema:"environment_name"`
-	HierarchyIdentifier string            `tfschema:"hierarchy_identifier"`
-	Tags                map[string]string `tfschema:"tags"`
+	Name                string               `tfschema:"name"`
+	ResourceGroupName   string               `tfschema:"resource_group_name"`
+	Location            string               `tfschema:"location"`
+	EnvironmentName     string               `tfschema:"environment_name"`
+	HierarchyIdentifier string               `tfschema:"hierarchy_identifier"`
+	AwsEnvironmentData  []AwsEnvironmentData `tfschema:"aws_environment_data"`
+	Tags                map[string]string    `tfschema:"tags"`
+}
+
+type AwsEnvironmentData struct {
+	OrganizationalDataMaster                  []OrganizationalDataMaster `tfschema:"organizational_data_master"`
+	OrganizationalDataMemberParentHierarchyId string                     `tfschema:"organizational_data_member_parent_hierarchy_id"`
+	Regions                                   []string                   `tfschema:"regions"`
+	ScanInterval                              int                        `tfschema:"scan_interval"`
+}
+
+type OrganizationalDataMaster struct {
+	StacksetName       string   `tfschema:"stackset_name"`
+	ExcludedAccountIds []string `tfschema:"excluded_account_ids"`
 }
 
 var _ sdk.Resource = SecurityCenterSecurityConnectorResource{}
@@ -58,15 +71,77 @@ func (r SecurityCenterSecurityConnectorResource) Arguments() map[string]*plugins
 		"location": commonschema.Location(),
 
 		"environment_name": {
-			Type:         pluginsdk.TypeString,
-			Required:     true,
-			ValidateFunc: validation.StringInSlice(securityconnectors.PossibleValuesForEnvironmentType(), false),
+			Type:     pluginsdk.TypeString,
+			Required: true,
+			ValidateFunc: validation.StringInSlice([]string{
+				string(securityconnectors.CloudNameAWS),
+				string(securityconnectors.CloudNameAzureDevOps),
+				string(securityconnectors.CloudNameGCP),
+				string(securityconnectors.CloudNameGitLab),
+				string(securityconnectors.CloudNameGithub),
+			}, false),
 		},
 
 		"hierarchy_identifier": {
 			Type:         pluginsdk.TypeString,
 			Required:     true,
 			ValidateFunc: validation.StringIsNotEmpty,
+		},
+
+		"aws_environment_data": {
+			Type:     pluginsdk.TypeList,
+			Optional: true,
+			MaxItems: 1,
+			Elem: &pluginsdk.Resource{
+				Schema: map[string]*pluginsdk.Schema{
+					"organizational_data_master": {
+						Type:     pluginsdk.TypeList,
+						Optional: true,
+						MaxItems: 1,
+						Elem: &pluginsdk.Resource{
+							Schema: map[string]*pluginsdk.Schema{
+								"stackset_name": {
+									Type:         pluginsdk.TypeString,
+									Optional:     true,
+									ValidateFunc: validation.StringIsNotEmpty,
+								},
+
+								"excluded_account_ids": {
+									Type:     pluginsdk.TypeList,
+									Optional: true,
+									Elem: &pluginsdk.Schema{
+										Type:         pluginsdk.TypeString,
+										ValidateFunc: validation.StringIsNotEmpty,
+									},
+								},
+							},
+						},
+						ConflictsWith: []string{"aws_environment_data.0.organizational_data_member_parent_hierarchy_id"},
+					},
+
+					"organizational_data_member_parent_hierarchy_id": {
+						Type:          pluginsdk.TypeString,
+						Optional:      true,
+						ValidateFunc:  validation.StringIsNotEmpty,
+						ConflictsWith: []string{"aws_environment_data.0.organizational_data_master"},
+					},
+
+					"regions": {
+						Type:     pluginsdk.TypeList,
+						Optional: true,
+						Elem: &pluginsdk.Schema{
+							Type:         pluginsdk.TypeString,
+							ValidateFunc: validation.StringIsNotEmpty,
+						},
+					},
+
+					"scan_interval": {
+						Type:         pluginsdk.TypeInt,
+						Optional:     true,
+						ValidateFunc: validation.IntBetween(1, 24),
+					},
+				},
+			},
 		},
 
 		"tags": commonschema.Tags(),
@@ -110,12 +185,14 @@ func (r SecurityCenterSecurityConnectorResource) Create() sdk.ResourceFunc {
 				Tags: pointer.To(model.Tags),
 			}
 
-			if model.EnvironmentName == string(securityconnectors.EnvironmentTypeAzureDevOpsScope) {
+			if model.EnvironmentName == string(securityconnectors.CloudNameAzureDevOps) {
 				parameters.Properties.EnvironmentData = securityconnectors.AzureDevOpsScopeEnvironmentData{}
-			} else if model.EnvironmentName == string(securityconnectors.EnvironmentTypeGithubScope) {
+			} else if model.EnvironmentName == string(securityconnectors.CloudNameGithub) {
 				parameters.Properties.EnvironmentData = securityconnectors.GithubScopeEnvironmentData{}
-			} else if model.EnvironmentName == string(securityconnectors.EnvironmentTypeGitlabScope) {
+			} else if model.EnvironmentName == string(securityconnectors.CloudNameGitLab) {
 				parameters.Properties.EnvironmentData = securityconnectors.GitlabScopeEnvironmentData{}
+			} else if model.EnvironmentName == string(securityconnectors.CloudNameAWS) {
+				parameters.Properties.EnvironmentData = expandAwsEnvironmentData(model.AwsEnvironmentData)
 			}
 
 			if _, err := client.CreateOrUpdate(ctx, id, parameters); err != nil {
@@ -157,6 +234,10 @@ func (r SecurityCenterSecurityConnectorResource) Read() sdk.ResourceFunc {
 				if props := model.Properties; props != nil {
 					state.EnvironmentName = string(pointer.From(props.EnvironmentName))
 					state.HierarchyIdentifier = pointer.From(props.HierarchyIdentifier)
+
+					if v, ok := props.EnvironmentData.(securityconnectors.AwsEnvironmentData); ok {
+						state.AwsEnvironmentData = flattenAwsEnvironmentData(v)
+					}
 				}
 			}
 
@@ -187,18 +268,20 @@ func (r SecurityCenterSecurityConnectorResource) Update() sdk.ResourceFunc {
 
 			if metadata.ResourceData.HasChange("environment_name") {
 				parameters.Properties.EnvironmentName = pointer.To(securityconnectors.CloudName(model.EnvironmentName))
-
-				if model.EnvironmentName == string(securityconnectors.EnvironmentTypeAzureDevOpsScope) {
-					parameters.Properties.EnvironmentData = securityconnectors.AzureDevOpsScopeEnvironmentData{}
-				} else if model.EnvironmentName == string(securityconnectors.EnvironmentTypeGithubScope) {
-					parameters.Properties.EnvironmentData = securityconnectors.GithubScopeEnvironmentData{}
-				} else if model.EnvironmentName == string(securityconnectors.EnvironmentTypeGitlabScope) {
-					parameters.Properties.EnvironmentData = securityconnectors.GitlabScopeEnvironmentData{}
-				}
 			}
 
 			if metadata.ResourceData.HasChange("hierarchy_identifier") {
 				parameters.Properties.HierarchyIdentifier = pointer.To(model.HierarchyIdentifier)
+			}
+
+			if model.EnvironmentName == string(securityconnectors.CloudNameAzureDevOps) {
+				parameters.Properties.EnvironmentData = securityconnectors.AzureDevOpsScopeEnvironmentData{}
+			} else if model.EnvironmentName == string(securityconnectors.CloudNameGithub) {
+				parameters.Properties.EnvironmentData = securityconnectors.GithubScopeEnvironmentData{}
+			} else if model.EnvironmentName == string(securityconnectors.CloudNameGitLab) {
+				parameters.Properties.EnvironmentData = securityconnectors.GitlabScopeEnvironmentData{}
+			} else if model.EnvironmentName == string(securityconnectors.CloudNameAWS) {
+				parameters.Properties.EnvironmentData = expandAwsEnvironmentData(model.AwsEnvironmentData)
 			}
 
 			if metadata.ResourceData.HasChange("tags") {
@@ -232,4 +315,81 @@ func (r SecurityCenterSecurityConnectorResource) Delete() sdk.ResourceFunc {
 			return nil
 		},
 	}
+}
+
+func expandAwsEnvironmentData(input []AwsEnvironmentData) *securityconnectors.AwsEnvironmentData {
+	if len(input) == 0 {
+		return &securityconnectors.AwsEnvironmentData{}
+	}
+
+	awsEnvironmentData := input[0]
+
+	result := &securityconnectors.AwsEnvironmentData{
+		Regions: pointer.To(awsEnvironmentData.Regions),
+	}
+
+	if v := awsEnvironmentData.OrganizationalDataMaster; v != nil {
+		result.OrganizationalData = expandAwsOrganizationalDataMaster(v)
+	} else if v := awsEnvironmentData.OrganizationalDataMemberParentHierarchyId; v != "" {
+		result.OrganizationalData = expandAwsOrganizationalDataMember(v)
+	}
+
+	if v := awsEnvironmentData.ScanInterval; v != 0 {
+		result.ScanInterval = pointer.To(int64(awsEnvironmentData.ScanInterval))
+	}
+
+	return result
+}
+
+func expandAwsOrganizationalDataMaster(input []OrganizationalDataMaster) *securityconnectors.AwsOrganizationalDataMaster {
+	awsOrganizationalDataMaster := input[0]
+
+	result := &securityconnectors.AwsOrganizationalDataMaster{
+		ExcludedAccountIds: pointer.To(awsOrganizationalDataMaster.ExcludedAccountIds),
+		StacksetName:       pointer.To(awsOrganizationalDataMaster.StacksetName),
+	}
+
+	return result
+}
+
+func expandAwsOrganizationalDataMember(input string) *securityconnectors.AwsOrganizationalDataMember {
+	result := &securityconnectors.AwsOrganizationalDataMember{
+		ParentHierarchyId: pointer.To(input),
+	}
+
+	return result
+}
+
+func flattenAwsEnvironmentData(input securityconnectors.AwsEnvironmentData) []AwsEnvironmentData {
+	result := make([]AwsEnvironmentData, 0)
+
+	awsEnvironmentData := AwsEnvironmentData{
+		Regions:      pointer.From(input.Regions),
+		ScanInterval: int(pointer.From(input.ScanInterval)),
+	}
+
+	if v, ok := input.OrganizationalData.(securityconnectors.AwsOrganizationalDataMaster); ok {
+		awsEnvironmentData.OrganizationalDataMaster = flattenAwsOrganizationalDataMaster(v)
+	} else if v, ok := input.OrganizationalData.(securityconnectors.AwsOrganizationalDataMember); ok {
+		awsEnvironmentData.OrganizationalDataMemberParentHierarchyId = flattenAwsOrganizationalDataMember(v)
+	}
+
+	return append(result, awsEnvironmentData)
+}
+
+func flattenAwsOrganizationalDataMaster(input securityconnectors.AwsOrganizationalDataMaster) []OrganizationalDataMaster {
+	result := make([]OrganizationalDataMaster, 0)
+
+	awsOrganizationalDataMaster := OrganizationalDataMaster{
+		ExcludedAccountIds: pointer.From(input.ExcludedAccountIds),
+		StacksetName:       pointer.From(input.StacksetName),
+	}
+
+	return append(result, awsOrganizationalDataMaster)
+}
+
+func flattenAwsOrganizationalDataMember(input securityconnectors.AwsOrganizationalDataMember) string {
+	organizationalDataMemberParentHierarchyId := pointer.From(input.ParentHierarchyId)
+
+	return organizationalDataMemberParentHierarchyId
 }
