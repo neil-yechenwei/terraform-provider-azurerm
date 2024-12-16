5393d3ae4381600fda861f127bc48aa052de3f7e
neil-yechenwei <yechenwei2007@hotmail.com>
2024-12-16 13:01:12 +0800

    update tc

--- internal/services/workloads/workloads_sap_three_tier_virtual_instance_resource_test.go
+++ internal/services/workloads/workloads_sap_three_tier_virtual_instance_resource_test.go
@@ -177,11 +177,13 @@ resource "azurerm_virtual_network" "test" {
 }
 
 resource "azurerm_subnet" "test" {
-  name                 = "acctest-subnet-%d"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.Storage"]
+  name                                          = "acctest-subnet-%d"
+  resource_group_name                           = azurerm_resource_group.test.name
+  virtual_network_name                          = azurerm_virtual_network.test.name
+  address_prefixes                              = ["10.0.2.0/24"]
+  private_endpoint_network_policies             = "Disabled"
+  private_link_service_network_policies_enabled = true
+  service_endpoints                             = ["Microsoft.Storage"]
 }
 
 resource "azurerm_resource_group" "app" {
